0be9b12a781d2e95aa6356d936aed4f1206348e4
jokoer <83909159+jokoer@users.noreply.github.com>
2023-09-28 13:47:11 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { PropsWithChildren } from 'react';
 import 'styles/main.css';
 
 const meta = {
-  title: 'Next.js Subscription Starter',
-  description: 'Brought to you by Vercel, Stripe, and Supabase.',
+  title: 'cpqy.org | payment solutions',
+  description: 'A Las Vegas, Nevada based organization.  ',
   cardImage: '/og.png',
   robots: 'follow, index',
   favicon: '/favicon.ico',
